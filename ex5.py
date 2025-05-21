aeb9fb17af0c83856409beba0abc98c71b6fffb4
Caio Eduardo <xavierrocha.caioeduardo@gmail.com>
2025-05-21 00:03:03 -0300

    Delete ex5.py

--- ex5.py
+++
@@ -1,9 +0,0 @@
-import random
-
-palavras = ['Vini', 'Nicolas', 'Marina', 'Mariana', 'Luisa', 'Paralelepipedo']
-
-curta = min(palavras, key=len)
-longa = max(palavras, key=len)
-
-print(f"Palavra mais longa: {longa}")
-print(f"Palavra mais curta: {curta}")
